dafaadafd753f6d0fb98ddc1814609303a57c33b
hugopijnenborg <hugopijnenborg@gmail.com>
2026-09-09 11:50:49 +0200

    Fix unclickable UI by removing MarketContextBridge from global layout

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 import './globals.css';
 import './marketintel-overrides.css';
 import PortfolioBridge from './portfolio/PortfolioBridge';
-import MarketContextBridge from './MarketContextBridge';
 
 export const metadata = {
   title: 'Stock Scanner',
@@ -9,5 +8,16 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  return <html lang="en"><head><link rel="stylesheet" href="/widget-fix.css" /><link rel="stylesheet" href="/readability.css" /></head><body><PortfolioBridge /><MarketContextBridge />{children}</body></html>;
+  return (
+    <html lang="en">
+      <head>
+        <link rel="stylesheet" href="/widget-fix.css" />
+        <link rel="stylesheet" href="/readability.css" />
+      </head>
+      <body>
+        <PortfolioBridge />
+        {children}
+      </body>
+    </html>
+  );
 }
